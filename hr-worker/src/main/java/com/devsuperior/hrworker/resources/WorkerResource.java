3561d72617b44d9e4057841febc51ca16f5fe54a
gisaklc <gisaklc@hotmail.com>
2021-09-02 14:33:13 -0300

    project hr-eureka-server created

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -42,10 +42,8 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) throws ObjectNotFoundException {
-		
 		/*** imprimir o numero da porta rodando**/
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
 		Worker worker = workService.findById(id);
 		return ResponseEntity.ok().body(worker);
 	}
